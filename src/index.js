12e0493077d082b2061550cde36c8112277c3cf6
Genci23 <genckaziaj@yahoo.com>
2022-06-22 01:08:42 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,20 @@
+/*
+Description
+We're going to create a Pokemon Trading Cards show page. To achieve this, you'll use JS to dynamically create all the elements on the page!
+
+Instructions
+- Use this template as the starting point => https://codesandbox.io/s/pokemon-trading-cards-template-rd5vq?file=/index.js
+- Make sure you check and understand the data that is given to you! 
+- Create a card using JS that represents a single pokemon, use the example image as a reference. You will also find an HTML example in the templates folder
+- Use the exact CSS classes you see in the example HTML to obtain the same style for each card
+- The cards should be nested inside <ul class="cards"></ul>. Otherwise the styles will break.
+- Use the official-artwork object key as the images for the cards. The images are all inside of the sprites key, in each pokemon object
+- Render all the cards on the page that represents all the pokemons, recreating the same layout, using JS
+
+Tips
+- Use functions to your advantage. Break each card into smaller parts, and think how you can turn them into functions.
+- Once you've got a card, think of a way to programmatically create all the others
+- Make sure you understand well the data structure before starting to create the cards
+*/
+
+console.log(data);
